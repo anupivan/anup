c3664dceefa58c1c7af532ef2ca9d868135ea6e3
anupivan <43266926+anupivan@users.noreply.github.com>
2018-11-16 14:59:57 +0530

    Add files via upload

---
+++ speech2.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr  
+
+# get audio from the microphone                                                                       
+r = sr.Recognizer()                                                                                   
+with sr.Microphone() as source: 
+	r.adjust_for_ambient_noise(source,duration=5)
+	r.dynamic_energy_threshold = True
+	print("Speak:")
+	audio = r.listen(source)   
+
+try:
+    print("You said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
